fd5c355d0313b0183916142bb3b6a8259bcf96f8
William Wold <wm@wmww.sh>
2020-07-08 14:55:21 -0400

    Add FlRenderer::create_display() virtual method

--- shell/platform/linux/fl_renderer.h
+++ shell/platform/linux/fl_renderer.h
@@ -52,6 +52,9 @@ struct _FlRendererClass {
   void (*set_window)(FlRenderer* renderer,
                      GdkWindow* window);
 
+  // Virtual method to create a new EGL display
+  EGLDisplay (*create_display)(FlRenderer* renderer);
+
   // Virtual method called when Flutter needs a surface to render to.
   EGLSurfacePair (*create_surface)(FlRenderer* renderer,
                                    EGLDisplay display,
